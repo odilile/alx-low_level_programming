43538a642a4f8eaba3eb8af46fcaffb975705d3f
odilile <achebopatience@gmail.com>
2022-06-30 21:05:02 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Enteyr point
+ * main - Prints "Programming is like building a multilingual puzzle
  *
- * Return: Always 0 (Success)
+ * Return: Zero(0) for Success
  */
 int main(void)
 {
-	puts("\Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
