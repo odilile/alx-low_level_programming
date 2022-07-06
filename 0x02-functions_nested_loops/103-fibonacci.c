dba1dda00c13446deca84adf042f6c6450fd510d
odilile <achebopatience@gmail.com>
2022-07-05 21:19:36 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the sum of even-valued Fibonacci sequence.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sumOffib;
+	float sum;
+
+	while (1)
+	{
+		sumOffib = fib1 + fib2;
+		if (sumOffib > 4000000)
+			break;
+
+		if ((sumOffib % 2) == 0)
+			sum += sumOffib;
+
+		fib1 = fib2;
+		fib2 = sumOffib;
+	}
+	printf("%.0f\n", sum);
+
+	return (0);
+}
