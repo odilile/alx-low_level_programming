b01afe718fed1615b8d59bf67d4ebfc129068aee
odilile <achebopatience@gmail.com>
2022-07-05 21:35:05 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * fibb - recurssively return fibonacci numbers.
+ * @i: the fibonacci number.
+ *
+ * Return: The fibonacci number.
+ */
+int fibb(unsigned int i)
+{
+	if (i == 1)
+		return (1);
+
+	if (i == 2)
+		return (2);
+
+	return (fibb(i - 1) + fibb(i - 2));
+}
+
+/**
+ * main - print fibonacci numbers between 1 and 98.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int i, f;
+
+	for (i = 1; i <= 98; i++)
+	{
+		f = fibb(i);
+		printf("%d", f);
+		if (i != 98)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
