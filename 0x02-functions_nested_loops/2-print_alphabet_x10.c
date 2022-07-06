a5bc56c1b142d8db66c08c8093db0a26bfda5d4a
odilile <achebopatience@gmail.com>
2022-07-05 17:02:39 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,20 @@
  *
  * Return: Always 0(Success)
  */
-
 void print_alphabet_x10(void)
 {
-	char c, i;
+	int i = 0;
 
-	for (i = 0; i <= 9; i++)
+	while (i <= 9)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		int c = 'a';
+
+		while (c <= 'z')
 		{
 			_putchar(c);
+			c++;
 		}
 		_putchar('\n');
+		i++;
 	}
 }
