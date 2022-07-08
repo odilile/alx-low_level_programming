3410130318a7b659e2f6f70574b8bccc948d655b
odilile <achebopatience@gmail.com>
2022-07-08 08:21:33 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,17 @@
  * @c: first parameter.
  * Return: 1 if upper 0 otherwise.
  */
+
 int _isupper(int c)
 {
-	if ((c >= 65) && (c <= 90))
-		return (1);
-	else
-		return (0);
+if (c >= 'A' && c <= '2')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
 }
