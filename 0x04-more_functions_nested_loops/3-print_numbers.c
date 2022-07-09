0b8aabb14b39656c20a1461393569c9c2b5db3bd
odilile <achebopatience@gmail.com>
2022-07-08 20:31:47 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the number zero to nine
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+int i;
+
+i = 0;
+while(i < 10)
+{
+_putchar(i + '0');
+i++
+}
+_putchar ('\n');
+}
