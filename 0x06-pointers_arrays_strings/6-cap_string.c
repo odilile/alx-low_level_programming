0d0751998c81481d6081fd05e45d26d0b5603ddc
odilile <achebopatience@gmail.com>
2022-07-14 16:03:13 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,20 +7,20 @@
 
 char *cap_string(char *s)
 {
-	int j = 0, i;
+	int j = 0, count;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + j) >= 97 && *(s + j) <= 122)
-		*(s + j) = *(s + j) - 32;
-	j++
-	while (*(s + j) != '\0')
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++
+	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + j) == sep_words[i])
+			if (*(s + count) == sep_words[i])
 			{
-				if ((*(s + (j + 1)) >= 97) && (*(s + (j + 1)) <= 122))
-					*(s + (j + 1)) = *(s + (j + 1)) - 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
 		}
