daabdabf5084c029523dcf481b20f67d8dc23f28
odilile <achebopatience@gmail.com>
2022-08-02 05:27:32 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#ifndef LINE_H
+#define LINE_H
+
+#include <stdio.h>
+/**
+ * main - prints the name of the file.
+ *
+ * Return: SUCCESS (0).
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
+#endif /* LINE_H */
